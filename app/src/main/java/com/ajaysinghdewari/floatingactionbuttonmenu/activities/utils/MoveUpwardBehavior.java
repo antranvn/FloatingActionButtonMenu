f14d6ff634d5f76e4b9312921c97a7950f909a5d
andrew <andevstudioth@gmail.com>
2020-04-05 08:14:38 +0700

    Migrate to Android X

--- app/src/main/java/com/ajaysinghdewari/floatingactionbuttonmenu/activities/utils/MoveUpwardBehavior.java
+++ app/src/main/java/com/ajaysinghdewari/floatingactionbuttonmenu/activities/utils/MoveUpwardBehavior.java
@@ -1,9 +1,9 @@
 package com.ajaysinghdewari.floatingactionbuttonmenu.activities.utils;
 
 import android.content.Context;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.ViewCompat;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
